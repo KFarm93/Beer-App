ccf3ce2955011fa8c695e6e5918d43dad96cd1ad
Robert Dunn <robertdunn220@gmail.com>
2016-11-28 13:40:14 -0500

    Trying to fix stuff

--- server.py
+++ server.py
@@ -1,56 +1,11 @@
-import requests
 from flask import Flask, render_template, redirect, request, session, flash, jsonify
-
-DEFAULT_BASE_URI = "http://api.brewerydb.com/v2"
-BASE_URI = ""
-API_KEY = "2197e5ac270cdce51585dbf484297b1f"
-
-simple_endpoints = ["beers", "breweries", "categories", "events",
-                    "featured", "features", "fluidsizes", "glassware",
-                    "locations", "guilds", "heartbeat", "ingredients",
-                    "search", "search/upc", "socialsites", "styles"]
-
-single_param_endpoints = ["beer", "brewery", "category", "event",
-                          "feature", "glass", "guild", "ingredient",
-                          "location", "socialsite", "style", "menu"]
-
-
-class BreweryDb:
-
-    @staticmethod
-    def __make_simple_endpoint_fun(name):
-        @staticmethod
-        def _function(options={}):
-            return BreweryDb._get("/" + name, options)
-        return _function
-
-    @staticmethod
-    def __make_singlearg_endpoint_fun(name):
-        @staticmethod
-        def _function(id, options={}):
-            return BreweryDb._get("/" + name + "/" + id, options)
-        return _function
-
-    @staticmethod
-    def _get(request, options):
-        options.update({"key" : BreweryDb.API_KEY})
-        return requests.get(BreweryDb.BASE_URI + request, params=options).json()
-
-    @staticmethod
-    def configure(apikey, baseuri=DEFAULT_BASE_URI):
-        BreweryDb.API_KEY = apikey
-        BreweryDb.BASE_URI = baseuri
-        for endpoint in simple_endpoints:
-            fun = BreweryDb.__make_simple_endpoint_fun(endpoint)
-            setattr(BreweryDb, endpoint.replace('/', '_'), fun)
-        for endpoint in single_param_endpoints:
-            fun = BreweryDb.__make_singlearg_endpoint_fun(endpoint)
-            setattr(BreweryDb, endpoint.replace('/', '_'), fun)
+from BreweryDB import BreweryDb
+brewerydb = BreweryDb()
 
 app = Flask('beer_trader', static_url_path = '')
 @app.route('/beer/<name>')
 def beerCall(name):
-    data = BreweryDB.__make_simple_endpoint_fun(name)
-    print data
+    print 'stuff'
+    return brewerydb.__make_simple_endpoint_fun(name)
 
 app.run(debug=True)
