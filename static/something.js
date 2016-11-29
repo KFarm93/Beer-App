36ad1448db8b6f3896b72eccbc7f228a7680ecfe
Robert Dunn <robertdunn220@gmail.com>
2016-11-29 09:38:39 -0500

    Added search functionality

--- static/something.js
+++ static/something.js
@@ -16,9 +16,9 @@ app.factory("BeerAPI", function factoryFunction($http, $cookies, $rootScope){
     });
   };
 
-  service.displayBeer = function(beer_name) {
+  service.displayResults = function(search_term) {
     return $http({
-      url: '/beer/' + beer_name
+      url: '/beer/' + search_term
     });
   };
   return service;
@@ -60,11 +60,11 @@ app.controller('BreweryController', function($scope, BeerAPI, $cookies, $rootSco
     $scope.brewerySearch($scope.page_num);
 });
 
-app.controller('BeerController', function($scope, BeerAPI, $cookies, $rootScope, $stateParams) {
-    var beer_name = $stateParams.beer_name;
-    console.log($stateParams.beer_name);
-    BeerAPI.displayBeer(beer_name).success(function(results) {
-      $scope.results = results;
+app.controller('SearchController', function($scope, BeerAPI, $cookies, $rootScope, $stateParams) {
+    $scope.search_term = $stateParams.search_term;
+    console.log($scope.search_term);
+    BeerAPI.displayResults($scope.search_term).success(function(results) {
+      $scope.results = results.data;
       console.log($scope.results);
     });
 });
@@ -90,9 +90,9 @@ app.config(function($stateProvider, $urlRouterProvider){
       controller: 'BeersController'
     })
     .state({
-      name: 'beer',
-      url: '/beer/{beer_name}',
-      templateUrl: 'beer.html',
-      controller: 'BeerController'
+      name: 'results',
+      url: '/search/{search_term}',
+      templateUrl: 'results.html',
+      controller: 'SearchController'
     });
 });
