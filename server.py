20388e4ab33063d7868ecf436bc1497f165281fe
Robert Dunn <robertdunn220@gmail.com>
2016-11-28 19:29:27 -0500

    This is a commit

--- server.py
+++ server.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, redirect, request, session, flash, jsonify
-from BreweryDB import BreweryDb
+from BreweryDB import *
+
+DEFAULT_BASE_URI = "http://api.brewerydb.com/v2"
+BASE_URI = ""
+API_KEY = "2197e5ac270cdce51585dbf484297b1f"
+
 brewerydb = BreweryDb()
+brewerydb.configure(API_KEY, DEFAULT_BASE_URI)
 
 app = Flask('beer_trader', static_url_path = '')
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
 
-@app.route('/beer/<name>')
-def beerCall(name):
-    print 'stuff'
-    return brewerydb.__make_simple_endpoint_fun(name)
+@app.route('/beers/<beer_name>')
+def beerCall(beer_name):
+    data = brewerydb.beers({'name':beer_name})
+    return jsonify(data)
 
-app.run(debug=True)
+@app.route('/beer/<page_num>')
+def beerCall(page_num):
+    data = brewerydb.beers({'p':page_num})
+    return jsonify(data)
+
+@app.route('/breweries/<page_num>')
+def breweryCall(page_num):
+    data = brewerydb.breweries({'p':page_num})
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/something.js
+++ static/something.js
@@ -1,22 +1,58 @@
 var app = angular.module('BeerApp', ['ui.router', 'ngCookies']);
 
-app.factory("Commerce_api", function factoryFunction($http, $cookies, $rootScope){
+app.factory("BeerAPI", function factoryFunction($http, $cookies, $rootScope){
   var service = {};
-  service.displayBeer = function(name){
+  $cookies.put('page_count', 0);
+  $cookies.put('beer_page_count', 0);
+  service.displayBeer = function(page_num){
     return $http({
-      url: '/beer/' + name
+      url: '/beer/' + page_num
     });
   };
+
+  service.displayBreweries = function(page_num){
+    return $http({
+      url: '/breweries/' + page_num
+    });
+  };
+
   return service;
 });
 
-app.controller('HomeController', function($scope, Commerce_api, $cookies, $rootScope){
-  $scope.name = 'Budweiser';
-  console.log('stuff');
-    Commerce_api.displayBeer($scope.name).success(function(results){
-      $scope.results = results;
-      console.log("Here", $scope.results);
-    });
+app.controller('BeerController', function($scope, BeerAPI, $cookies, $rootScope, $stateParams){
+  $scope.beerSearch = function() {
+    var beer_page_count = $cookies.get('beer_page_count');
+    if (beer_page_count === 0) {
+      $cookies.put('beer_page_num', 1);
+    }
+    else if (beer_page_count > 0) {
+      $cookies.put('beer_page_num', $scope.beer_page_num);
+    }
+      BeerAPI.displayBeer($cookies.get('beer_page_num')).success(function(results){
+        $scope.results = results.data;
+        $cookies.put('beer_page_count', 1);
+      });
+  };
+  $scope.beerSearch($scope.beer_page_num);
+});
+
+app.controller('BreweryController', function($scope, BeerAPI, $cookies, $rootScope, $stateParams){
+
+    $scope.brewerySearch = function() {
+      var page_count = $cookies.get('page_count');
+      if (page_count === 0) {
+        $cookies.put('page_num', 1);
+      }
+      else if (page_count > 0) {
+        $cookies.put('page_num', $scope.page_num);
+      }
+
+      BeerAPI.displayBreweries($cookies.get('page_num')).success(function(results){
+        $scope.results = results.data;
+        $cookies.put('page_count', 1);
+      });
+    };
+    $scope.brewerySearch($scope.page_num);
 });
 
 app.config(function($stateProvider, $urlRouterProvider){
@@ -26,5 +62,23 @@ app.config(function($stateProvider, $urlRouterProvider){
       url : '/home',
       templateUrl: 'frontpage.html',
       controller: 'HomeController'
-  });
+    })
+    .state({
+      name: 'breweries',
+      url: '/breweries',
+      templateUrl: 'breweries.html',
+      controller: 'BreweryController'
+    })
+    .state({
+      name: 'beers',
+      url: '/beers',
+      templateUrl: 'beers.html',
+      controller: 'BeerController'
+    })
+    .state({
+      name: 'beer',
+      url: '/beer',
+      template: 'beer.html',
+      controller: 'BeerController'
+    });
 });
